d8fab130f19d9580e6c5e53310f06e629e0b56ba
DrZin <61010707@kmitl.ac.th>
2019-07-04 22:39:13 +0700

    add SideNav Logout state

---
+++ src/components/SideNav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styled, { ThemeProvider } from "styled-components";
+import { themes, WindowContent } from "react95";
+import { StateContext } from "../../StateProvider/StateProvider";
+import { LOGOUT } from "../../StateProvider/actions_constant";
+export default () => {
+  let { state, dispatch } = React.useContext(StateContext);
+
+  const handalLogOut = e => {
+    dispatch({ type: LOGOUT });
+  };
+  return (
+    <div>
+      <button onClick={handalLogOut}>Log Out</button>
+      in side dev
+    </div>
+  );
+};
--- src/page/MainWindows.js
+++ src/page/MainWindows.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import { StateContext } from '../StateProvider/StateProvider'
-import { LOGOUT } from '../StateProvider/actions_constant'
+import { StateContext } from "../StateProvider/StateProvider";
+import { LOGOUT } from "../StateProvider/actions_constant";
 
-import Login from '../components/Login'
-
-const MainWindows = (props) => {
-    let { state, dispatch } = React.useContext(StateContext)
-    return (
-        <div>
-            {
-                state.isLogin ? <button onClick={
-                    e => dispatch({
-                        type: LOGOUT
-                    })
-                }>Log out</button> : <Login />
-            }
-        </div>
-    )
-}
-export default MainWindows
+import Login from "../components/Login";
+import SideNav from "../components/SideNav";
+const MainWindows = props => {
+  let { state, dispatch } = React.useContext(StateContext);
+  return (
+    <div>
+      {state.isLogin ? (
+        //  <button onClick={
+        //     e => dispatch({
+        //         type: LOGOUT
+        //     })
+        // }>Log out</button>
+        <SideNav />
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+};
+export default MainWindows;
