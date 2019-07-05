925f598db3469e4bdda98537076525177f4f331f
DrZin <61010707@kmitl.ac.th>
2019-07-05 07:43:17 +0700

    add image container as circle

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
 
 import styled, {ThemeProvider, createGlobalStyle} from 'styled-components'
-import {
-	themes,
-	WindowContent,
-	Window,
-	WindowHeader,
-	reset,
-	Divider
-} from 'react95'
+import {themes, WindowContent, Window, reset, Divider} from 'react95'
+import testImg from '../../resource/img/IMG_6413.jpg'
 
 import {StateContext} from '../../StateProvider/StateProvider'
 import {LOGOUT} from '../../StateProvider/actions_constant'
@@ -30,8 +24,29 @@ export default () => {
 			<button onClick={handalLogOut}>Log Out</button>
 			<ThemeProvider theme={themes.default}>
 				<Window style={{width: 350, height: '100vh'}}>
-					<WindowHeader>👗 Side_Nav.exe</WindowHeader>
-					<WindowContent>This is profile</WindowContent>
+					<WindowContent>
+						<div
+							style={{
+								width: '100px',
+								height: '100px',
+								position: 'relative',
+								overflow: 'hidden',
+								borderRadius: '50%'
+							}}>
+							<img
+								src={testImg}
+								alt="test img "
+								style={{
+									display: 'inline',
+									margin: '0 auto',
+									marginLeft: '-25%',
+									height: '100%',
+									width: 'auto'
+								}}
+							/>
+						</div>
+						This is profile
+					</WindowContent>
 					<Divider />
 					<WindowContent>This is Tree bar</WindowContent>
 				</Window>
