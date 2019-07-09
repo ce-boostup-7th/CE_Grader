02b0a554580f3a9b10b6649fb191f7f603b3d07d
DrZin-dev <pasawee.dev@gmail.com>
2019-07-09 16:18:10 +0700

    leader board complete

---
+++ src/page/LeaderPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import SideNav from '../../components/SideNav'
+import LeaderBoard from '../../components/LeaderBoard'
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: row;
+`
+export default () => {
+	return (
+		<Container>
+			<SideNav />
+			<LeaderBoard />
+		</Container>
+	)
+}
